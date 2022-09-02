d99a41b967d9d3c1be0a4f05da1c7ef694fd9892
Shaohan Hu <hushaohan@gmail.com>
2022-09-01 23:29:19 -0400

    Create hbm.py

---
+++ hbm.py
@@ -0,0 +1,47 @@
+from flask import Flask
+import json
+import click
+import time
+import requests
+import threading
+
+heartbeat_period: float
+action_url: str
+prev_heartbeat = time.time()
+
+
+def check_heartbeat():
+    global prev_heartbeat
+    threading.Timer(heartbeat_period, check_heartbeat).start()
+    gap = time.time() - prev_heartbeat
+    print(heartbeat_period, action_url)
+    if gap >= heartbeat_period:
+        print(f"dead: {gap}")
+        requests.get(action_url)
+
+
+@click.command()
+@click.option('--host', default='127.0.0.1')
+@click.option('--port', default=80)
+@click.option('--period', default=60)
+@click.option('--url', default="test_url")
+def create_app(host, port, period, url):
+    global heartbeat_period, action_url
+    heartbeat_period = period
+    action_url = url
+    check_heartbeat()
+    app = Flask(__name__)
+
+    @app.route("/")
+    @click.argument("url")
+    @click.argument("period")
+    def run():
+        global prev_heartbeat
+        prev_heartbeat = time.time()
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+    app.run(host=host, port=port)
+
+
+if __name__ == '__main__':
+    create_app()
